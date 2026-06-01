c97d6e3500934d8e5cfc28483d88b11e568cad8f
dilan102 <dilanmunos91@gmail.com>
2026-06-01 08:50:25 -0500

    act

--- lib/documents.ts
+++ lib/documents.ts
@@ -16,9 +16,9 @@ const ALLOWED_DOCUMENTS: Record<string, OfficialDocument> = {
     description:
       "Metodología Sinergia Cognitivo-Motriz, sesión tipo, pilares de trabajo y macrociclo de 8 semanas.",
     previewUrl:
-      "https://drive.google.com/file/d/1xwc1kk8BzHxsXg0Ntsxfdf_UkNmihop4/preview",
+      "https://drive.google.com/file/d/1BZAldiHjVIlezPojWZFgeI5fDKi6IrcI/preview",
     downloadUrl:
-      "https://drive.google.com/file/d/1xwc1kk8BzHxsXg0Ntsxfdf_UkNmihop4/view?usp=sharing",
+      "https://drive.google.com/file/d/1BZAldiHjVIlezPojWZFgeI5fDKi6IrcI/view?usp=sharing",
     type: "pdf",
   },
   "proyecto-2026": {
@@ -27,9 +27,9 @@ const ALLOWED_DOCUMENTS: Record<string, OfficialDocument> = {
     description:
       "Proyecto social, misión, visión, objetivos ODS, metodología por edades y plan de trabajo deportivo.",
     previewUrl:
-      "https://docs.google.com/document/d/170ynpkZxi_YgzFbt32yFhNmSyr6Acb-S/preview",
+      "https://docs.google.com/document/d/1gdbfCNCUaq-xNvLWxsTxFqwm9uY0GhRq/preview",
     downloadUrl:
-      "https://docs.google.com/document/d/170ynpkZxi_YgzFbt32yFhNmSyr6Acb-S/edit?usp=sharing",
+      "https://docs.google.com/document/d/1gdbfCNCUaq-xNvLWxsTxFqwm9uY0GhRq/edit?usp=drive_link&ouid=117261589495075090119&rtpof=true&sd=true",
     type: "docx",
   },
 };
