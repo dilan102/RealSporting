6f846746efcd12698d497860f77df8f21fd78bb1
dilan102 <dilanmunos91@gmail.com>
2026-07-30 20:59:25 -0500

    Remove preloader from layout

--- app/layout.tsx
+++ app/layout.tsx
@@ -7,7 +7,6 @@ import { ThemeProvider } from "@/components/ui/ThemeProvider";
 import { AIAssistantWidget } from "@/components/ui/AIAssistantWidget";
 import { AIAssistantFloatingButton } from "@/components/ui/AIAssistantFloatingButton";
 import { PageTransition } from "@/components/ui/PageTransition";
-import Preloader from "@/components/ui/Preloader";
 import Cursor from "@/components/ui/Cursor";
 import { club } from "@/lib/content";
 import { OG_IMAGE_URL, siteMetadata } from "@/lib/site";
@@ -47,14 +46,13 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="es" suppressHydrationWarning>
-      <head>
-        <link rel="stylesheet" href="/preloader.css" />
-      </head>
       <body className="min-h-screen flex flex-col">
-        <Preloader />
         <Cursor />
         <ThemeProvider>
-          <div id="site-content" className="site-content flex min-h-screen flex-col">
+          <div
+            id="site-content"
+            className="site-content flex min-h-screen flex-col"
+          >
             <Navbar />
             <AdminPortal />
             <GlobalContentManager />
